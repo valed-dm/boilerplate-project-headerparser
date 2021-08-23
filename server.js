0256ef3c8474a7a78af2611390cc1fdb2f8ca5b0
valed-dm <dmvaled@gmail.com>
2021-08-23 23:00:13 +0300

    Request Header Parser Microservice
    challenge successfully completed

--- server.js
+++ server.js
@@ -25,7 +25,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+//this is valed-dm API endpoint ...
+app.get("/api/whoami", function(req, res) {
+  console.log(req);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
